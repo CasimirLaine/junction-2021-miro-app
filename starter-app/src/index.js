0e856bb6a9f783c560571d42b9dfd8bf28c50e07
Ilkka Lappeteläinen <ilkka@Ilkkas-MacBook-Pro.local>
2021-11-20 00:05:20 +0200

    heroku push

--- starter-app/src/index.js
+++ starter-app/src/index.js
@@ -1,9 +1,16 @@
-const { board } = window.miro;
+import cors from 'cors'
+
 
 async function init() {
-  await board.ui.on("icon:click", async () => {
-    await board.ui.openPanel({ pageUrl: "app.html" });
+  await miro.board.ui.on("icon:click", async () => {
+    await miro.board.ui.openPanel({
+      // Absolute or relative URL of the page whose content you want to display inside the panel      
+      pageUrl: "app.html"
+    });
   });
 }
+const { board } = window.miro;
+const appdata = await board.getAppData("variables")
+
 
 init();
