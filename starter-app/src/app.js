372a73d9d4645dbdbf0c8d01b3edfc55d4631974
Casimir Laine <casimir.laine@gmail.com>
2021-11-20 17:00:33 +0200

    Working find and replace feature.

--- starter-app/src/app.js
+++ starter-app/src/app.js
@@ -4,26 +4,12 @@ const { board } = window.miro;
 
 async function init() {
   const data = {
-    variable: "variableName",
-    value: "variableValue"
   }
   console.log('board in app.js', board)
   await board.setAppData("variables", data)
   const appdata = await board.getAppData("variables")
   console.log('appdata', appdata)
-
-  printFunction()
-  const stickyNote = await board.createStickyNote({
-    content: "Hello, World from Ray, ornot, is it !",
-  });
-  console.log(stickyNote)
-
-  await board.viewport.zoomTo(stickyNote);
 }
 var variables = await findVariables()
 console.log(variables)
 init();
-const table = document.getElementById('vtable');
-console.log(table)
-
-
--- starter-app/src/table.js
+++ starter-app/src/table.js
@@ -8,7 +8,7 @@ function printFunction() {
     console.log("Print statement!");
 }
 
-function appendToTable(){
+function appendToTable() {
     id += 1
     var button = document.getElementById("b1");
     button.style.display = 'inline'
@@ -21,12 +21,12 @@ function appendToTable(){
     document.getElementById("valinput").value = ""
 
     var element = document.getElementById("vtable-body")
-    element.innerHTML += '<tr id='+id+'><td>' + variable + '</td><td>' + value + '</td><td scope="col"><button onclick="removeRow('+id+')" class="xbutton">✕</button></td></tr>';
+    element.innerHTML += '<tr id=' + id + '><td>' + variable + '</td><td>' + value + '</td><td scope="col"><button onclick="removeRow(' + id + ')" class="xbutton">✕</button></td></tr>';
     data[variable] = value
     saveVariables()
 }
 
-function showVariableForm(){
+function showVariableForm() {
     var button = document.getElementById("b1");
     button.style.display = 'none'
     var element = document.getElementById("input-field");
@@ -39,32 +39,33 @@ function removeRow(id) {
 
 function toggleVisibility(id, bool) {
     let el = document.getElementById(id);
-    if(bool) {
+    if (bool) {
         el.style.display = "inline"
     }
-    if(el.style.display === "none") {
+    if (el.style.display === "none") {
         el.style.display = "inline"
     } else {
         el.style.display = "none"
     }
 }
-function test(){
+function test() {
     promise.then(
         data => {
             data.findVariables().then(d => {
-            console.log('Print', d)
-            return d})
-            .then(d =>{
-                for (var key in d) {
-                    if (d.hasOwnProperty(key)) {
-                        var arr = d[key]
-                        for (var i = 0; i < arr.length; i++) {
-                            document.getElementById("varinput").value = arr[i]
-                            appendToTable()
+                console.log('Print', d)
+                return d
+            })
+                .then(d => {
+                    for (var key in d) {
+                        if (d.hasOwnProperty(key)) {
+                            var arr = d[key]
+                            for (var i = 0; i < arr.length; i++) {
+                                document.getElementById("varinput").value = arr[i]
+                                appendToTable()
+                            }
                         }
                     }
-                }
-            })
+                })
         }
     )
 }
@@ -72,10 +73,15 @@ function test(){
 function saveVariables() {
     console.log(data)
     console.log('board', board)
-    board.setAppData("variables", data).then(d=>{
-        var appdata = board.getAppData("variables").then(
-            d=> {
+    board.setAppData("variables", data).then(d => {
+        var variableTable = board.getAppData("variables").then(
+            d => {
                 console.log('appdata set', d)
+                promise.then(
+                    data => {
+                        data.setVariables();
+                    }
+                )
             }
         )
     })
