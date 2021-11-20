e84ec0e69daa776bf9dc20a5e8fe90a23bd606c4
Casimir Laine <casimir.laine@gmail.com>
2021-11-20 15:45:49 +0200

    Implemented variable filling.

--- starter-app/src/items.js
+++ starter-app/src/items.js
@@ -64,4 +64,42 @@ async function findVariables() {
     }
     return widgetVariables;
 }
+
+function widgetsWithVariable(variableName, widgetVariables) {
+    var widgetIds = new Array();
+    for (var widgetId in widgetVariables) {
+        var variableList = widgetVariables[widgetId]
+        if (variableList.includes(variableName)) {
+            widgetIds.push(widgetId);
+        }
+    }
+    return widgetIds;
+}
+
+function updateWidget(widget, variable, value) {
+    var itemType = itemTypes[widget.type];
+    for (var textField of itemType.textFields) {
+        widget[textField] = widget[textField].replace(variableSign + variable, value)
+    }
+    widget.sync()
+}
+
+async function setVariables() {
+    var widgetVariables = await findVariables();
+    var variables = board.getAppData('variables');
+    for (var key in variables) {
+        var widgetIds = widgetsWithVariable(key, widgetVariables);
+        for (var widgetId of widgetIds) {
+            var value = variables[key];
+            try {
+                var widget = await board.getById(widgetId);
+                updateWidget(widget, variable, value);
+            } catch (error) {
+                console.log(error);
+            }
+        }
+    }
+
+}
+
 export { findVariables };
