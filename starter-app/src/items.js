96e5639fd3fc85fce47fc7f1ee45451c15ab7efc
Casimir Laine <casimir.laine@gmail.com>
2021-11-20 12:05:21 +0200

    Implemented a way to fetch variables from any type and any field.

--- starter-app/src/items.js
+++ starter-app/src/items.js
@@ -2,16 +2,41 @@ const { board } = window.miro;
 const variableSign = "$"
 const regexPattern = "[" + variableSign + "]{1}[a-zA-Z0-9äöüÄÖÜ]+";
 
+const itemTypes = {
+    card: {
+        textFields: [
+            'title', 'description'
+        ]
+    }
+}
+
+function findVariablesInText(text) {
+    var variables = new Array();
+    const foundVariables = [...text.matchAll(regexPattern)];
+    for (var variable of foundVariables) {
+        variables.push(variable[0].substring(1));
+    }
+    return variables;
+}
+
+function findVariablesInObject(item, itemType) {
+    var variables = new Array();
+    for (var textField of itemType.textFields) {
+        const stringValue = item[textField]
+        const foundVariables = findVariablesInText(stringValue)
+        variables.push(...foundVariables)
+    }
+    return variables;
+}
+
 async function findVariables() {
     var variables = new Array();
-    let boardItems = await board.get({
-        type: ['card']
-    });
-    for (var boardItem of boardItems) {
-        let title = boardItem.title;
-        const foundVariables = [...title.matchAll(regexPattern)];
-        for (var variable of foundVariables) {
-            variables.push(variable[0].substring(1));
+    for (var itemType in itemTypes) {
+        let boardItems = await board.get({
+            type: [itemType]
+        });
+        for (var boardItem of boardItems) {
+            variables.push(...findVariablesInObject(boardItem, itemTypes[itemType]))
         }
     }
     return variables;
