bee0c576c129c17830520bbda654d5a6fe988303
atreyaray@gmail.com <atreyaray@gmail.com>
2021-11-20 10:03:25 +0200

    basic html variable table

--- starter-app/src/app.js
+++ starter-app/src/app.js
@@ -1,11 +1,17 @@
 const { board } = window.miro;
 
 async function init() {
+  console.log('hello world')
+  printFunction()
   const stickyNote = await board.createStickyNote({
-    content: "Hello, World!",
+    content: "Hello, World from Ray, ornot, is it !",
   });
 
   await board.viewport.zoomTo(stickyNote);
 }
 
 init();
+const table = document.getElementById('vtable');
+console.log(table)
+
+
---
+++ starter-app/src/table.js
@@ -0,0 +1,25 @@
+function printFunction() {
+    console.log("Print statement!");
+}
+
+function appendToTable(){
+    var button = document.getElementById("b1");
+    button.style.display = 'inline'
+    var element = document.getElementById("input-field");
+    element.style.display = 'None'
+
+    var variable = document.getElementById("varinput").value
+    var value = document.getElementById("valinput").value
+    document.getElementById("varinput").value = ""
+    document.getElementById("valinput").value = ""
+
+    var element = document.getElementById("vtable")
+    element.innerHTML += '<tr><td>' + variable + '</td><td>' + value + '</td></tr>';
+}
+
+function showVariableForm(){
+    var button = document.getElementById("b1");
+    button.style.display = 'None'
+    var element = document.getElementById("input-field");
+    element.style.display = 'inline'
+}
