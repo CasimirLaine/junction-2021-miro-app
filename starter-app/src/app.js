01f2657df38faadc5085349e7e1693b639d5219f
Casimir Laine <casimir.laine@gmail.com>
2021-11-20 22:45:31 +0200

    Fixed window.variables.

--- starter-app/src/app.js
+++ starter-app/src/app.js
@@ -1,7 +1,7 @@
 const { board } = window.miro;
 
 async function init() {
-  var variables = window.variables.variables;
+  var variables = window.variables;
   if (variables == null || variables == undefined || !variables) {
     console.log("Inited app data with empty variable table.")
     window.variables = {}
