337eaa5987b58c8e7245557029e2d03d15318c08
Matias <matiasnie2@gmail.com>
2021-11-20 21:20:34 +0200

    editing variables works

--- starter-app/src/table.js
+++ starter-app/src/table.js
@@ -4,7 +4,7 @@ let id = 0
 let data = {}
 // dictionary contains saved variables
 var dictionary = {}
-let variableNames
+let varBeingEdited
 
 function printFunction() {
     console.log("Print statement!");
@@ -29,9 +29,16 @@ function appendToTable() {
     document.getElementById("valinput").value = ""
 
 
-    editButton = '<button id="' + editId+'" style="float:right; display:none;" class="editButton xbutton" onmouseover="toggleVisibility(' + editId + ', true)">edit</button>'
+    editButton = '<button onclick="editVariable('+id+')" id="' + editId+'" style="float:right; display:none;" class="editButton xbutton" onmouseover="toggleVisibility(' + editId + ', true)">edit</button>'
     var element = document.getElementById("vtable-body")
-    element.innerHTML += '<tr id='+id+'><td onmouseover="toggleVisibility(\''+editId+'\')" onmouseout="toggleVisibility(\''+editId+'\')" scope="col">' + variable + " " + editButton +'</td><td>' + value + '</td><td scope="col"><button onclick="removeRow('+id+')" class="xbutton">✕</button></td></tr>';
+    element.innerHTML += '<tr id='+id+'>\
+                            <td onmouseover="toggleVisibility(\''+editId+'\')" onmouseout="toggleVisibility(\''+editId+'\')" scope="col">\
+                                <div style="margin: 0;" id="'+varId+'">' + variable + "</div> " + editButton +'</td><td id="'+valId+'">' + value + '\
+                            </td>\
+                            <td scope="col">\
+                                <button onclick="removeRow('+id+')" class="xbutton">✕</button>\
+                            </td>\
+                        </tr>';
     data[variable] = value
 }
 
@@ -52,12 +59,14 @@ function toggleVisibility(id, bool) {
     if (bool) {
         el.style.display = "inline"
     }
+    
     if (el.style.display === "none") {
         el.style.display = "inline"
     } else {
         el.style.display = "none"
     }
 }
+
 function test() {
     promise.then(
         data => {
@@ -78,6 +87,41 @@ function test() {
                 })
         }
     )
+}
+
+function editVariable(id) {
+    toggleVisibility("edit-field")
+    let valId = "val"+id
+    let varId = "var"+id
+
+    varBeingEdited = [valId, varId]
+
+    // let variable = document.getElementById(valId)
+    // let value = document.getElementById(varId)
+    // console.log(value, variable)
+    // console.log(value.innerHTML, variable.innerHTML)
+
+
+}
+
+function saveEdit() {
+    let valId = varBeingEdited[0]
+    let varId = varBeingEdited[1]
+
+    let variable = document.getElementById(valId)
+    let value = document.getElementById(varId)
+
+    let newVariable = document.getElementById("varedit")
+    let newValue = document.getElementById("valedit")
+    console.log(newValue, newVariable)
+
+    console.log(newValue.innerHTML, newVariable.innerHTML)
+
+    variable.innerHTML = newVariable.value
+    value.innerHTML = newValue.value
+
+    toggleVisibility("edit-field")
+
 }
 
 
